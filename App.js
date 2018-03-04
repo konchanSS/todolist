f0e6f5797f1911bcadc9da76df16d515ae25fd4f
konchanSS <b1447864@planet.kanazawa-it.ac.jp>
2018-03-04 16:43:15 +0900

    Add Sample Task

--- App.js
+++ App.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    Text,
+    View,
+    FlatList,
+    TextInput,
+    StyleSheet,
+    Platform,
+} from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+export default class MainContainer extends Component {
+    render() {
+        // データソース定義
+        let todos = [
+            { id: 1, title: "create some actions" },
+            { id: 2, title: "create some reducer" },
+            { id: 3, title: "create store" }
+        ];
+        // この部分はビューをレンダーです。
+        return (
+            <View style={styles.container}>
+                <FlatList data={todos}
+                          renderItem={this._renderItems}
+                          keyExtractor={(item, index) => index} />
+            </View>
+        );
+    }
+
+    // このメソッドでは一覧の各アイテムをレンダーするです。
+    _renderItems({ item }) {
+        return (
+            <View>
+                <Text>{item.title}</Text>
+            </View>
+        )
+    }
 }
 
+// ビューのスタイル修正
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        marginTop: Platform.OS === "ios" ? 20 : 0
+    }
 });
+
+AppRegistry.registerComponent('todolist', () => MainContainer);
