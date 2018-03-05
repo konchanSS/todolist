d57b90ea511c24c2498dcefc0298a8ab217f74c1
konchanSS <b1447864@planet.kanazawa-it.ac.jp>
2018-03-05 21:13:10 +0900

    super simple todo application

--- App.js
+++ App.js
@@ -1,23 +1,88 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    View,
+    TextInput,
+    Button,
+    FlatList,
+    Text,
+    TouchableOpacity
+} from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+            task: [],
+            key: 0
+        };
+    }
+
+    render() {
+        return (
+			<View style={{flex:1}}>
+                <View style={{flex:10, paddingTop:20, backgroundColor:'#DDDDDD'}}>
+                    <FlatList
+                        style={{flex:1}}
+                        data={this.state.task}
+                        renderItem={({item}) => <TouchableOpacity onPress={() => this._onPressCell(item.key)}><Text style={styles.item}>{item.text}</Text></TouchableOpacity>}
+                    />
+                </View>
+                <View style={{flex:1, flexDirection: 'row'}}>
+				<TextInput
+					style={{flex:3}}
+					placeholder="Type here to task"
+					onChangeText={(text) => this.setState({text})}
+					value={this.state.text}
+                />
+                    <View style={{flex:1, justifyContent:'center'}}>
+				<Button
+                    style={{flex:1}}
+					onPress={this._onPressAdd}
+					title='追加'
+                />
+                    </View>
+                </View>
+			</View>
+        );
+    }
+
+    _onPressCell = (key) => {
+        console.log(key);
+        this.state.task.some((value, index) => {
+            if(value.key == key) {
+                this.state.task.splice(index);
+            }
+        });
+        this.setState({task:this.state.task});
+        console.log(this.state.task);
+    }
+
+    _onPressAdd = () => {
+        this.state.task.push({key:this.state.key, text: this.state.text});
+        this.setState({text:'', key:this.state.key+1});
+        console.log(this.state.task);
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    list: {
+        flex:1,
+    },
+    textInput:{
+        flex:1,
+        height: 40,
+        padding: 20,
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+        backgroundColor:'white'
+    },
 });
